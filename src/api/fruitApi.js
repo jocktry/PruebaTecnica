70c27dfa2778c4771e1ddf52be4610ac095baf97
jocktry <dsfox27@gmail.com>
2025-03-06 20:30:00 -0500

    changes deploy

--- src/api/fruitApi.js
+++ src/api/fruitApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const fruitApi = createApi({
   reducerPath: 'fruitApi',
-  baseQuery: fetchBaseQuery({ baseUrl: '/api/fruit/' }), // Base URL debe ser '/api/fruit/'
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://www.fruityvice.com/api/fruit/' }), // Base URL debe ser '/api/fruit/'
   endpoints: (builder) => ({
     getFruits: builder.query({
       query: () => 'all', // Endpoint correcto
